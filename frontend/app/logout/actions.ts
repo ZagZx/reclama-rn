8660a8cf0bd15c663f90339389dee932d0d1d91a
Pedro Victor <azevedodvictor@gmail.com>
2025-12-05 15:19:18 -0300

    feat: alerta e redirect na página de logout

--- frontend/app/logout/actions.ts
+++ frontend/app/logout/actions.ts
@@ -2,14 +2,3 @@
 
 // import { redirect } from "next/navigation";
 
-export async function logout() {
-    const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-
-    const options = {
-        method: "POST",
-        credentials: "include" as RequestCredentials
-    }
-    const response = await fetch(`${apiUrl}/api/logout`, options);
-
-    return response;
-}
