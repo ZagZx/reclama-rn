9e387181a75d767d8d5faa0f042b4ba43333963c
ZagZx <azevedodvictor@gmail.com>
2025-12-17 23:13:35 -0300

    feat: página usuário agora exibe o nome do usuário

--- frontend/app/usuario/page.tsx
+++ frontend/app/usuario/page.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import Link from "next/link";
 import Button from "@/components/ui/Button";
+import { H1 } from "@/components/ui/titles";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Page() {
+  const {usuario} = useAuth();
+
   return (
-    <div className="flex flex-row gap-[20px] justify-center items-center py-10">
-      <Link href="/usuario/contestacoes">
-        <Button>
-          Minhas contestações  
-        </Button>
-      </Link>
-      <Link href="/usuario/reclamacoes">
-        <Button>
-          Minhas reclamações
-        </Button>
-      </Link>
-    </div>
+    <section className="flex flex-col flex-1 gap-10 items-center px-20 py-5">
+      <H1>Olá, {usuario?.nome}</H1>
+      <div className="flex gap-2">
+        <Link href="/usuario/contestacoes">
+          <Button>
+            Minhas contestações  
+          </Button>
+        </Link>
+        <Link href="/usuario/reclamacoes">
+          <Button>
+            Minhas reclamações
+          </Button>
+        </Link>
+      </div>
+    </section>
   );
 }
